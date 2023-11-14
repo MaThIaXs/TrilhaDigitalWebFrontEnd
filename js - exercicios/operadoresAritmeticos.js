dbe54d8b9e85f419adf59480f27e74451b44b46f
MaThIaXs <matheusferraz042@gmail.com>
2023-11-13 23:48:45 -0300

    Aula 3 - Operadores Aritmeticos

---
+++ js - exercicios/operadoresAritmeticos.js
@@ -0,0 +1,19 @@
+// Operadores Aritméticos
+
+let primeiroNumero = 40;
+let segundoNumero = 2;
+
+const soma = primeiroNumero + segundoNumero
+
+console.log('Soma = ', soma);
+console.log('Subtração = ', primeiroNumero - segundoNumero);
+console.log('Multiplicação = ', primeiroNumero * segundoNumero);
+console.log('Divisão = ', primeiroNumero / segundoNumero);
+console.log('Exponenciação = ', primeiroNumero ** segundoNumero);
+console.log('Resto da divisão = ', primeiroNumero % segundoNumero);
+
+console.log((3 + 7) * 3);
+console.log((10 + 9 + 10) / 3);
+
+console.log(Math.PI);
+console.log(Math.sqrt(9));
