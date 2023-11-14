a3e664f3c8bbb625285075f159d46f8e834c350b
MaThIaXs <matheusferraz042@gmail.com>
2023-11-13 23:33:54 -0300

    Aula 2 - Variaveis

---
+++ js - exercicios/variaveis.js
@@ -0,0 +1,31 @@
+// Variáveis
+
+let idade = 18; //criando a primeira variável
+
+console.log(idade);
+
+// tipos de variáveis
+
+let numero = 30; //number
+let altura = 1.82; //number
+let nome = 'Matheus'; //string
+let estudando = true; //boolean
+
+console.log(numero, typeof numero);
+console.log(altura, typeof altura);
+console.log(nome, typeof nome);
+console.log(estudando, typeof estudando);
+
+let outra;
+
+console.log(outra);
+
+let nula = null;
+
+console.log(nula);
+
+var variavel = 20; //sintaxe antiga, não utilizar
+
+const constante = 100;
+
+console.log(constante)
