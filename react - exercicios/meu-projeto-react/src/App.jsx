7e5b6e01f4c67c0ce21ce8b4fc76fe360e9b6c76
MaThIaXs <matheusferraz042@gmail.com>
2023-11-15 13:03:06 -0300

    Aula 4 - Estados e Eventos

--- react - exercicios/meu-projeto-react/src/App.jsx
+++ react - exercicios/meu-projeto-react/src/App.jsx
@@ -1,23 +1,26 @@
 import MeuComponente from './components/MeuComponente'
+import MeuContador from './components/MeuContador'
 
 function App() {
   return (
     <>
       <h1>Olá Mundo, React!</h1>
       <MeuComponente />
-      <MeuBotao conteudo='me clique' />
+      {/* <MeuBotao conteudo='me clique' />
       <MeuBotao conteudo='depois aqui' />
-      <MeuBotao conteudo='por fim aqui' />
+      <MeuBotao conteudo='por fim aqui' /> */}
+      <hr></hr>
+      <MeuContador />
     </>
   )
 }
 
-function MeuBotao(props) {
-  return (
-    <button>
-      {props.conteudo}
-    </button>
-  )
-}
+// function MeuBotao(props) {
+//   return (
+//     <button>
+//       {props.conteudo}
+//     </button>
+//   )
+// }
 
 export default App
