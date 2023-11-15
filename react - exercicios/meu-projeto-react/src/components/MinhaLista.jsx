b2db76c5c95bc86390598315388029fe53eb2117
MaThIaXs <matheusferraz042@gmail.com>
2023-11-15 14:01:47 -0300

    Aula 7 - Efeitos Colaterais

--- react - exercicios/meu-projeto-react/src/components/MinhaLista.jsx
+++ react - exercicios/meu-projeto-react/src/components/MinhaLista.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react"
+
 const lista = [
   {id: '1', value: 'Fruta'},
   {id: '2', value: 'Legume'},
@@ -5,11 +7,39 @@ const lista = [
 ]
 
 export default function MinhaLista() {
-  return lista.map((item) => {
-    return(
-      <div key={item.id}>
-        <h4>{item.value}</h4>
-      </div>
-    )
-  })
+  
+  const [produtos, setProdutos] = useState(lista);
+  const [pesquisa, setPesquisa] = useState('');
+
+  useEffect( //toda vez que tiver uma reenderização do componente o useEffect é executado
+    () => {
+      if(pesquisa) { //se a pesquisa for verdadeira atualiza para a novaLista
+        const novaLista = lista.filter((item) => { //permite criar uma lista nova somente com os elementos que eu quero com base em uma condição
+          return item.value.toLowerCase().includes(pesquisa.toLowerCase());//para cada item pega o valor e verifica se o valor dele inclui o que esta na pesquisa
+        })
+        setProdutos(novaLista);
+      } else { 
+        setProdutos(lista)
+      }
+
+    }, [pesquisa]//executa o efeito colateral quando a pesquisa se altera
+  )
+
+  return(
+    <div>
+      <h1>Efeitos colaterais</h1>
+      <input 
+        value={pesquisa} //valor do input vai ser a variavel que irei definir
+        onChange={(e) => setPesquisa(e.target.value)} //permite ouvir a mudança do valor do input definindo a pesquisa com o valor que esta sendo digitado
+        placeholder="pesquise aqui..."></input>
+
+      {produtos.map((item) => {
+        return(
+          <div key={item.id}>
+            <h4>{item.value}</h4>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
