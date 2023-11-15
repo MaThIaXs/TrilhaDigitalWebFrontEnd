29ee35a075ab9bd8a1a7968314515b9014bd6746
MaThIaXs <matheusferraz042@gmail.com>
2023-11-15 13:17:40 -0300

    Aula 5 - Renderização Condicional

--- react - exercicios/meu-projeto-react/src/components/MeuContador.jsx
+++ react - exercicios/meu-projeto-react/src/components/MeuContador.jsx
@@ -2,17 +2,32 @@ import { useState } from "react";
 
 export default function MeuContador() {
 
-    const [contador, setContador] = useState(0);
+  const [contador, setContador] = useState(0);
 
-    function aumentar() {
-        setContador(contador + 1);
-    }
+  function aumentar() {
+    setContador(contador + 1);
+  }
 
-    return(
-        <div>
-            <h1>Meu Contador</h1>
-            <h3>{contador}</h3>
-            <button onClick={aumentar}>Aumentar</button>
-        </div>
-    )
+  function diminuir() {
+    setContador(contador - 1);
+  }
+
+	if(contador > 10) {
+		return(
+			<div>
+				<h1>Valor muito alto! {contador}</h1>
+				<button onClick={aumentar}>Aumentar</button>
+        <button onClick={diminuir}>Diminuir</button>
+			</div>
+		)
+	}
+
+  return(
+    <div>
+      <h1>Meu Contador: {contador}</h1>
+
+      <button onClick={aumentar}>Aumentar</button>
+      <button onClick={diminuir}>Diminuir</button>
+    </div>
+  )
 }
